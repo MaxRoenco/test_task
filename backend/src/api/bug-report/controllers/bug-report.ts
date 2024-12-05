dbe98a3bb13bb43b9bf614de27dcb5e0b373bed4
MaxRoenco <maxroenco2004@gmail.com>
2024-12-05 22:10:45 +0200

    Strapi implementation

---
+++ backend/src/api/bug-report/controllers/bug-report.ts
@@ -0,0 +1,7 @@
+/**
+ * bug-report controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::bug-report.bug-report');
---
+++ backend/src/api/bug-report/services/bug-report.ts
@@ -0,0 +1,7 @@
+/**
+ * bug-report service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::bug-report.bug-report');
