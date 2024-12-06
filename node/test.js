df56006c92b3ea615e454fb95eb8c3a4510cb678
IslamAbukoush <islam.abu.koush@isa.utm.md>
2024-12-06 12:38:04 +0200

    documentId

--- node/test.js
+++ node/test.js
@@ -1,57 +1,13 @@
-const messages = [
-    {
-        "id": 1,
-        "user": {
-            "id": 1,
-            "name": "Nick"
-        },
-        "text": "Hello, is the problem fixed?",
-        "timestamp": "2024-12-01T12:40:00Z"
-    },
-    {
-        "id": 2,
-        "user": {
-            "id": 2,
-            "name": "Alice Johnson",
-            "role": "Frontend Developer"
-        },
-        "text": "We're looking into this issue and will update shortly.",
-        "timestamp": "2024-12-01T14:00:00Z"
-    }
-]
-
-async function pushData(url, data) {
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        body: data.stringify(),
-      });
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.error(error.message);
-    }
+async function getData(url, cityId, foodTypeId) {
+  try {
+    const response = await strapi.query("image").find({
+      where: { city: cityId, foodType: foodTypeId },
+      populate: true,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
+}
 
-  async function getData(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  messages.forEach( message => {
-    pushData("http://localhost:1337/api/messages", message);
-    getData("http://localhost:1337/api/messages");
-  })
+console.log(getData())
