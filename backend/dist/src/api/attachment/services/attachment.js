5666820b010ec9524cb56067bbbacd850363f77e
MaxRoenco <maxroenco2004@gmail.com>
2024-12-06 10:26:30 +0200

    strapi config

---
+++ backend/dist/src/api/attachment/services/attachment.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * attachment service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::attachment.attachment');
---
+++ backend/dist/src/api/bug-report/services/bug-report.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * bug-report service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::bug-report.bug-report');
